c26d0880447a93779b63faff7197490158c28fc7
iti-D <105444975+iti-D@users.noreply.github.com>
2022-05-12 18:47:28 +0300

    Update libreddit.py

--- libreddit.py
+++ libreddit.py
@@ -1,10 +1,10 @@
-#import pyperclip
+import pyperclip
 
-def change_link(link):
+def change_link(link): # changes the link
     return "https://libredd.it" + link.split('reddit.com').pop()
 
-old_link = input("your URL here: ")
+old_link = pyperclip.waitForPaste() # saves the last copied element (text) from clipboard to old_link.
 new_link = change_link(old_link)
-#pyperclip.copy(new_link)
+pyperclip.copy(new_link) # automatically copies new link to your clipboard.
 print(f"new link was copied to your clipboard: {new_link}")
 input("any input to close task")
